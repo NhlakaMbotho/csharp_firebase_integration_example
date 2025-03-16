34165636671cb8d13b2b040bee1200a16d4ff16d
elvis_munch <nhlakambotho@gmail.com>
2025-03-17 01:34:52 +0200

    init

---
+++ AppMain/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Firebase;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            var fbUtils = new FirebaseUtils();
+
+            string response = "";
+
+            try
+            {
+                response = await fbUtils.SendMessageAsync("Test message to android device", "foo_bar");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed: {0}", ex.ToString());
+            }
+
+            Console.WriteLine("Successfully send message to firebase: {0}", response);
+        }
+    }
+}
+
---
+++ FirebaseUtils/FirebaseUtils.cs
@@ -0,0 +1,40 @@
+﻿namespace Firebase;
+
+using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+
+
+public class FirebaseUtils
+{
+    private FirebaseApp _firebaseInstance;
+
+    public FirebaseUtils()
+    {
+        _firebaseInstance = FirebaseApp.Create(new AppOptions()
+        {
+            Credential = GoogleCredential.FromFile("./travel-smart-service-account-key.json")
+        });
+    }
+
+    public FirebaseApp firebaseInstance => _firebaseInstance;
+
+    public Task<string> SendMessageAsync(string messageText, string topic)
+    {
+
+        var messageObject = new Message()
+        {
+            Data = new Dictionary<string, string> () {
+                { "alert", "Storm is coming" }
+            },
+            Notification = new Notification()
+            {
+                Body = messageText,
+            },
+            Topic = topic
+        };
+
+        return FirebaseMessaging.GetMessaging(firebaseInstance).SendAsync(messageObject);
+    }
+}
